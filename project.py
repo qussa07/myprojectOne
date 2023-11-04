c8362c9c8701848430eff0deecb0d243ebb9a9e2
qussa07 <148780123+qussa07@users.noreply.github.com>
2023-11-04 18:28:08 +0300

    добавил много разных фишек

--- project.py
+++ project.py
@@ -1,9 +1,10 @@
 import io
 import sys
-import datetime
+import os
+from datetime import date
 import openpyxl
-from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog
-from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from PyQt5 import uic, QtCore
 
 template = '''<?xml version="1.0" encoding="UTF-8"?>
 <ui version="4.0">
@@ -36,7 +37,7 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
    <widget class="QPushButton" name="save_button">
     <property name="geometry">
      <rect>
-      <x>230</x>
+      <x>140</x>
       <y>30</y>
       <width>281</width>
       <height>31</height>
@@ -50,7 +51,7 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
     <property name="geometry">
      <rect>
       <x>50</x>
-      <y>70</y>
+      <y>100</y>
       <width>659</width>
       <height>78</height>
      </rect>
@@ -71,6 +72,16 @@ template = '''<?xml version="1.0" encoding="UTF-8"?>
       </widget>
      </item>
     </layout>
+   </widget>
+   <widget class="QDateEdit" name="select_date">
+    <property name="geometry">
+     <rect>
+      <x>500</x>
+      <y>40</y>
+      <width>110</width>
+      <height>22</height>
+     </rect>
+    </property>
    </widget>
   </widget>
   <widget class="QMenuBar" name="menubar">
@@ -97,9 +108,14 @@ class Redactor(QMainWindow):
         super().__init__()
         f = io.StringIO(template)
         uic.loadUi(f, self)
+        self.setWindowTitle('Массовое изменение xlsx файлов')
+        self.db_data = None
+        self.return_real_date()
         self.db_button.clicked.connect(self.open_db)
+        self.db_button.setEnabled(False)
         self.file_button.clicked.connect(self.open_file)
         self.save_button.clicked.connect(self.redactor)
+        self.save_button.setEnabled(False)
 
     def open_file(self):
         self.dialog = QFileDialog.getOpenFileName(self, 'Открыть путь к файлу', '', filter='Лист Excel (*.xlsx)')[0]
@@ -109,10 +125,18 @@ class Redactor(QMainWindow):
             self.active_data = self.data.active
         except Exception:
             self.statusBar().showMessage('Не указан файл')
+            self.error_message('Ошибка!', 'Не указан нужный файл')
             return
+        self.check()
 
     def save(self, i):
-        self.data.save(('/'.join(self.dialog.split('/')[0:-1]) + '/' + i + '.xlsx'))
+        directory = os.path.abspath(i)
+        if not(directory):
+            os.mkdir(directory)
+        date = self.select_date.dateTime().toString('dd-MM-yyyy')
+        self.data.save(directory + '/' + f'{date}-sm.xlsx')
+        self.statusBar().showMessage('Успешно!')
+        self.information_message('Успешно!', 'Создание и изменение файлов прошло успешно.')
 
     def open_db(self):
         self.dialog_db = QFileDialog.getOpenFileName(self, 'Открыть путь', '', filter='Блокнот (*.txt)')[0]
@@ -122,7 +146,9 @@ class Redactor(QMainWindow):
                 self.db_data = file.read().split(';')
         except Exception:
             self.statusBar().showMessage('Не указан файл')
+            self.error_message('Ошибка!', 'Не указан нужный файл')
             return
+        self.check()
 
     def redactor(self):
         try:
@@ -131,14 +157,44 @@ class Redactor(QMainWindow):
                 return
             for i in self.db_data:
                 self.active_data['B1'] = i
-                self.active_data['J1'] = datetime.date.today()
+                date_from_QDateEdit = self.select_date.dateTime().toString('dd.MM.yyyy')
+                self.active_data['J1'] = date_from_QDateEdit
                 self.save(i)
 
 
         except Exception as file:
-            print(file)
+            self.error_message('Ошибка!', 'Не указан файл')
             self.statusBar().showMessage('Не указан файл')
             return
+
+    def return_real_date(self):
+        year = int(str(date.today()).split('-')[0])
+        day = int(str(date.today()).split('-')[1])
+        mouth = int(str(date.today()).split('-')[2])
+        self.select_date.setDate(QtCore.QDate(year, day, mouth))
+        self.select_date.setDisplayFormat("dd.MM.yyyy")
+
+    def check(self):
+        print(1)
+        if self.data:
+            self.db_button.setEnabled(True)
+            if self.db_data:
+                self.save_button.setEnabled(True)
+
+    def error_message(self, title, message):
+        error_dialog = QMessageBox(self)
+        error_dialog.setIcon(QMessageBox.Critical)
+        error_dialog.setWindowTitle(title)
+        error_dialog.setText(message)
+        error_dialog.exec_()
+
+    def information_message(self, title, message):
+        error_dialog = QMessageBox(self)
+        error_dialog.setIcon(QMessageBox.Information)
+        error_dialog.setWindowTitle(title)
+        error_dialog.setText(message)
+        error_dialog.exec_()
+
 
 
 if __name__ == '__main__':
